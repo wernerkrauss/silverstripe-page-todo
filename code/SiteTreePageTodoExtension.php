ce6f11de68ac13edfcd74cdbd160b23ba6a054e5
Werner Krauss <werner.krauss@netwerkstatt.at>
2015-05-26 11:47:52 +0200

    hotfix: better check if ToDo field actually exists

--- code/SiteTreePageTodoExtension.php
+++ code/SiteTreePageTodoExtension.php
@@ -7,7 +7,7 @@ class SiteTreePageTodoExtension extends DataExtension {
 	);
 	
 	function updateCMSFields(FieldList $fields) {
-		if (!$fields->hasField('ToDo')) {
+		if (!$fields->dataFieldByName('ToDo')) {
 			$fields->addFieldToTab('Root', new Tab(_t('SiteTree.TABTODO', 'To-do') . ($this->owner->ToDo ? '**' : ''),
 				new LiteralField("ToDoHelp", _t('SiteTree.TODOHELP', "<p>You can use this to keep track of work that needs to be done to the content of your site.  To see all your pages with to do information, open the 'Site Reports' window on the left and select 'To Do'</p>")),
 				new TextareaField("ToDo", "", 10)
